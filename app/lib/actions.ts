fb83d3879f2285cf7877ebcb5a2cc88db22b655f
sakshikhalode22 <sakshikhalode321@gmail.com>
2025-09-25 18:44:27 +0530

    minor change

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -75,14 +75,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  try {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
-  } catch (error) {
-    console.error(error);
-      return {
-        message: 'Database Error: Failed to Delete Invoice.',
-        type: 'error',
-      };
-  }
 }
